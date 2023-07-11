23169e6eec624991a4549671ce8d7072b2fd450c
Rainsoda <79522444+Rainsoda@users.noreply.github.com>
2023-07-11 15:56:52 +0000

    Created accounts app

--- online_shop/django_project/settings.py
+++ online_shop/django_project/settings.py
@@ -35,12 +35,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local
+    "accounts.apps.AccountsConfig"
 ]
 
 MIDDLEWARE = [
