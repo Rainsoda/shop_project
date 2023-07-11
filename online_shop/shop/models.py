d6f1509b1de4da0c624944b9d88bee19bf78a94c
Rainsoda <79522444+Rainsoda@users.noreply.github.com>
2023-07-11 16:53:17 +0000

    Added Product model in shop/models.py

--- online_shop/shop/models.py
+++ online_shop/shop/models.py
@@ -25,3 +25,40 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(
+        upload_to="products/%Y/%m/%d", 
+        blank=True
+    )
+    description = models.TextField(blank=True)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["id", "slug"]),
+            models.Index(fields=["name"]),
+            models.Index(fields=["-created"])
+        ]
+
+    def __str__(self):
+        return self.name
