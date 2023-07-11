3392c2fa36523ddf54bd87e64aecd8cffd411e69
Rainsoda <79522444+Rainsoda@users.noreply.github.com>
2023-07-11 16:45:07 +0000

    Added Category model in shop/models.py

--- online_shop/shop/models.py
+++ online_shop/shop/models.py
@@ -1,3 +1,27 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(
+        max_length=200, 
+        unique=True
+    )
+
+    class Meta:
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["name"])
+        ]
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
