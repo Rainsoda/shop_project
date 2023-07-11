ba0ebc44c3a19dac47a10b2910c1fce4ca091d99
Rainsoda <79522444+Rainsoda@users.noreply.github.com>
2023-07-11 16:16:42 +0000

    Modified accounts/admin.py

--- online_shop/accounts/admin.py
+++ online_shop/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import (
+    CustomUserCreationForm,
+    CustomUserChangeForm
+)
+
+CustomUser = get_user_model()
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["id", "username", "email", "is_active", "is_staff", "is_superuser"]
+
+    add_from = CustomUserCreationForm
+    add_fieldsets = UserAdmin.add_fieldsets
+
+    form = CustomUserChangeForm
+    fieldsets = UserAdmin.fieldsets
