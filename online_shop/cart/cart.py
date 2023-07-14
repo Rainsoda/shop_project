9f70ae064405dc83d4259f1ebfd99afcb3a9ad3e
Rainsoda <79522444+Rainsoda@users.noreply.github.com>
2023-07-14 19:06:38 +0000

    Added remove function to the cart class in cart/cart.py

--- online_shop/cart/cart.py
+++ online_shop/cart/cart.py
@@ -15,7 +15,7 @@ class Cart:
 
     def add(self, product, quantity=1, override_quantity=False):
         """Add a product to the cart or update its quantity."""
-        product_id = str(product_id)
+        product_id = str(product.id)
 
         if product_id not in self.cart:
             self.cart[product_id] = {"quantity": 0, "price": str(product.price)}
@@ -30,3 +30,11 @@ class Cart:
     def save(self):
         # Mark the session as "modified" to make sure it gets saved
         self.session.modified = True
+
+    def remove(self, product):
+        """Remove a product from the cart."""
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
